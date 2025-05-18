1bf3c6e9ef9396c3de85edb4421c0ddc2b6aab5c
yunhoJ <wjse0213@naver.com>
2025-05-19 03:52:10 +0900

    🎉  Init: Next.js Tailwind shadcn을 사용한 블로그 프로젝트

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import globals from 'globals';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +13,22 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  js.configs.recommended,
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      // 여기에 추가적인 규칙을 설정할 수 있습니다
+      'no-unused-vars': 'warn',
+      'no-console': 'warn',
+    },
+  },
+  eslintConfigPrettier,
 ];
 
 export default eslintConfig;
