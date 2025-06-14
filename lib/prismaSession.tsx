f7779e84ab94af45964d56e86e650bfcba5a65df
yunhoJ <wjse0213@naver.com>
2025-06-14 20:47:22 +0900

    ✨ 추가: Radix UI 및 Axios 의존성 추가 - package.json 및 package-lock.json에 @radix-ui/react-dialog, axios, cmdk 의존성 추가 - prisma/schema.prisma 파일에서 BlogPost 모델에 postDraft 필드에 대한 유니크 제약 조건 추가 - lib/prismaSession.tsx 파일에서 PrismaClient의 import 경로 수정

--- lib/prismaSession.tsx
+++ lib/prismaSession.tsx
@@ -1,5 +1,5 @@
 // lib/prisma.ts
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@/lib/generated/prisma';
 
 const globalForPrisma = globalThis as unknown as {
 	prisma: PrismaClient | undefined;
